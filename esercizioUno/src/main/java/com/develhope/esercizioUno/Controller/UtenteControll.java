62309d70f7c3811396a521550628275011aacfc9
Pierluigi <rolletto1@gmail.com>
2025-06-05 18:27:16 +0200

    primo esercizio

---
+++ esercizioUno/src/main/java/com/develhope/esercizioUno/Controller/UtenteControll.java
@@ -0,0 +1,17 @@
+package com.develhope.esercizioUno.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class UtenteControll {
+    @GetMapping("v1/ciao")
+    public String saluto (@RequestParam (required = false,defaultValue = "Giuseppe") String nome, @RequestParam (required = false,defaultValue = "Lombardia")  String provincia) {
+        return "Ciao " + nome + " com'è il tempo in " + provincia + "?" ;
+
+    }
+}
